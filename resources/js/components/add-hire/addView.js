a4b90b037f374ba71c57ff2639c982c57e59b70f
jplute7 <jaceplute@gmail.com>
2019-08-31 11:31:10 -0500

    Minor Clean-Up
    Removing unused imports and changing directory name.

--- resources/js/components/add-hire/addView.js
+++ resources/js/components/add-hire/addView.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import './addView.css';
 import AddHire from './addHire';
 import Slide from '@material-ui/core/Slide';
--- resources/js/components/dashboard/dashboard-main/expanded-view-modal/expandedView.js
+++ resources/js/components/dashboard/dashboard-main/expanded-view-modal/expandedView.js
@@ -8,14 +8,10 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-//import DialogContentText from '@material-ui/core/DialogContentText';
 import Button from '@material-ui/core/Button';
-import Paper from '@material-ui/core/Paper';
-import PropTypes from 'prop-types';
 
 
 import './expandedView.css';
-import { Divider, Hidden } from '@material-ui/core';
 
 class Employee{
 
