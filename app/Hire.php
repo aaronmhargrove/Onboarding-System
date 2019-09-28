1b819145011e7aefe8a0b6ed6368628bb4047a30
jplute7 <jaceplute@gmail.com>
2019-09-28 13:40:23 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/searching' into JACE_APP_REFACTORS""
    This reverts commit ff5b6e1fa0c234ce155b0987ce57d15440d1b388.

--- app/Hire.php
+++ app/Hire.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Hire extends Model{
+    use Searchable;
+
     protected $guarded = []; // accept all
 
     public function manager(){
@@ -16,10 +19,32 @@ class Hire extends Model{
     }
 
     public function hireSteps(){
-        return $this->hasMany(HireStep::class, 'hire_id');
+        return $this->hasMany(HireStep::class)
+            ->with('step')
+            ->join('steps AS step', 'step.id', 'hire_steps.step_id')
+            ->orderBy('step.order', 'asc')
+        ;
     }
 
     public function hireType(){
         return $this->has(HireType::class);
+    }
+
+    public function getCurrentStep() {
+        $incompleteSteps = [];
+
+        // Just return the first incomplete step since they are ordered on the relationship
+        foreach ($this->hireSteps as $hireStep) {
+            if ($hireStep->status == 0) {
+                return $hireStep;
+            }
+        }
+
+        return null;
+    }
+
+    public function searchableAs()
+    {
+        return 'hires';
     }
 }
