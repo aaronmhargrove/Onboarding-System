5e0b8bf6127223924af5e1fb99f0d9dafd554cd0
Aaron Hargrove <aaronmh98@outlook.com>
2019-11-05 16:41:57 -0600

    CSRF change

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -21,6 +21,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'http://127.0.0.1:8000/*',
         'localhost:8000/*',
-        'http://localhost:8000/*'
+        'http://localhost:8000/*',
+        '*'
     ];
 }
